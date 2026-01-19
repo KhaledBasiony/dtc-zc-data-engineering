407eb0d3c89c9853d59850eeb2a2a405fd32f055
Khaled Basiony <basiony.khaled@outlook.com>
2026-01-19 08:51:08 +0200

    modified launcher to accept and pass arguments to docker run

--- py-docker-launcher.sh
+++ py-docker-launcher.sh
@@ -1,3 +1,4 @@
-docker run --rm -it python:3.13.11-slim bash -c 'pip --version'
+docker run --rm -it python:3.13.11-slim $@
 
+# to check pip version lauch with -> bash -c 'pip --version'
 # pip 25.3 from /usr/local/lib/python3.13/site-packages/pip (python 3.13)
